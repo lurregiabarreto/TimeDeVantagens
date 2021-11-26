7ddad209b79e3cd6623ab649086b458b7b956ee5
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-26 13:14:03 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/GerenciadorCompeticoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciadorCompeticoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCompeticoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorCompeticoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/gerenciadorCompeticoes/GerenciadorCompeticoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciadorCompeticoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorCompeticoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
