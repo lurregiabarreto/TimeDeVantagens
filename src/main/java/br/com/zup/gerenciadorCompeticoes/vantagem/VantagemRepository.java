5b561a2d5d1f0f53706b26af2d0b46f7429541e5
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-29 15:12:27 -0300

    Adicionar método para encontrar por beneficio

--- src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/VantagemRepository.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/VantagemRepository.java
@@ -3,4 +3,7 @@ package br.com.zup.gerenciadorCompeticoes.vantagem;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VantagemRepository extends CrudRepository<Vantagem,String> {
+
+    Vantagem findByBeneficio(String beneficio);
+
 }
