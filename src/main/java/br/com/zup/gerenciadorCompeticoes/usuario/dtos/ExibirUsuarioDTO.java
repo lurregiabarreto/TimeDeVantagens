5b1da24c933ad5a032e83ee243e76f78aa1bec21
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-29 12:04:20 -0300

    Criar dto para exibir o usuario

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/dtos/ExibirUsuarioDTO.java
@@ -0,0 +1,47 @@
+package br.com.zup.gerenciadorCompeticoes.usuario.dtos;
+
+import br.com.zup.gerenciadorCompeticoes.enuns.Time;
+
+public class ExibirUsuarioDTO {
+
+    private String email;
+    private String nomeUsuario;
+    private Time timeCoracao;
+    private int pontos;
+
+    public ExibirUsuarioDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public Time getTimeCoracao() {
+        return timeCoracao;
+    }
+
+    public void setTimeCoracao(Time timeCoracao) {
+        this.timeCoracao = timeCoracao;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+
+}
