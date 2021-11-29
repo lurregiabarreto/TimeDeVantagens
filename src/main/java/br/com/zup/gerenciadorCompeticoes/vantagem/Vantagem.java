e0e8e70c6a9ce6527f8aa63f5a3ae09d03a3df7b
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-29 11:07:22 -0300

    Modelar classe Model Vantagem-1

--- src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/Vantagem.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/Vantagem.java
@@ -1,7 +1,18 @@
 package br.com.zup.gerenciadorCompeticoes.vantagem;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vantagem")
 public class Vantagem {
+
+    @Id
+    @Column(nullable = false)
     private String beneficio;
+    @Column(nullable = false)
     private double pontos;
 
     public Vantagem() {
