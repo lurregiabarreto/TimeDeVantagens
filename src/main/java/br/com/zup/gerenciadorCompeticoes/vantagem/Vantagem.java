50ea217bc152adaf3ba00cee9c190ccd7769f3ab
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-29 10:55:38 -0300

    Modelar classe Model Vantagem

--- src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/Vantagem.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/Vantagem.java
@@ -1,6 +1,27 @@
 package br.com.zup.gerenciadorCompeticoes.vantagem;
 
 public class Vantagem {
+    private String beneficio;
+    private double pontos;
+
+    public Vantagem() {
+    }
+
+    public String getBeneficio() {
+        return beneficio;
+    }
+
+    public void setBeneficio(String beneficio) {
+        this.beneficio = beneficio;
+    }
+
+    public double getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
+    }
 
 }
 
