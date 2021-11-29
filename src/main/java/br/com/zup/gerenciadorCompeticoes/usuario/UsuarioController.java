a5fc2df6d658269eb96b69c30e7bca05c0bd76b8
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-29 12:08:59 -0300

    Adicionar método para cadastrar usuário

--- src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioController.java
@@ -1,9 +1,11 @@
 package br.com.zup.gerenciadorCompeticoes.usuario;
 
+import br.com.zup.gerenciadorCompeticoes.usuario.dtos.CadastroUsuarioDTO;
+import br.com.zup.gerenciadorCompeticoes.usuario.dtos.ExibirUsuarioDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -13,5 +15,14 @@ public class UsuarioController {
     UsuarioService usuarioService;
     @Autowired
     ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ExibirUsuarioDTO cadastrarUsuario(@RequestBody CadastroUsuarioDTO cadastroRecebido){
+        Usuario usuario = usuarioService.salvarUsuario(modelMapper.map(cadastroRecebido,Usuario.class));
+
+        return modelMapper.map(usuario,ExibirUsuarioDTO.class);
+    }
+
 
 }
