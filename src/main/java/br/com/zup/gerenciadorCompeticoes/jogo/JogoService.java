d888e6ea2d90bfe9bdcf54ad0767841238c07857
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-29 13:53:45 -0300

    Adicionar método para salvar jogo

--- src/main/java/br/com/zup/gerenciadorCompeticoes/jogo/JogoService.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/jogo/JogoService.java
@@ -3,10 +3,17 @@ package br.com.zup.gerenciadorCompeticoes.jogo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class JogoService {
 
     @Autowired
     JogoRepository jogoRepository;
+
+    public Jogo salvarJogo(Jogo jogoRecebido){
+        jogoRecebido.setDataDeCadastro(LocalDateTime.now());
+        return jogoRepository.save(jogoRecebido);
+    }
 
 }
