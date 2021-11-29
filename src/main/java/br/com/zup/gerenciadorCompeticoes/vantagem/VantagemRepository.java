88138da1aadef39784e87940e40d363de32a47f6
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-29 11:39:07 -0300

    Criar repository para vantagem

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/VantagemRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.gerenciadorCompeticoes.vantagem;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VantagemRepository extends CrudRepository<Vantagem,String> {
+}
