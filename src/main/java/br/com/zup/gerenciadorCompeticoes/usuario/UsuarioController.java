96c24936d7c11716620a1185b31899469c129e2e
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-30 11:53:36 -0300

    Adicionar metodo Exibir Todos Jogos Controller

--- src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioController.java
@@ -1,11 +1,17 @@
 package br.com.zup.gerenciadorCompeticoes.usuario;
 
+import br.com.zup.gerenciadorCompeticoes.jogo.Jogo;
+import br.com.zup.gerenciadorCompeticoes.jogo.JogoService;
+import br.com.zup.gerenciadorCompeticoes.jogo.dtos.ExibirDetalheJogoDTO;
 import br.com.zup.gerenciadorCompeticoes.usuario.dtos.CadastroUsuarioDTO;
 import br.com.zup.gerenciadorCompeticoes.usuario.dtos.ExibirUsuarioDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -18,11 +24,20 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ExibirUsuarioDTO cadastrarUsuario(@RequestBody CadastroUsuarioDTO cadastroRecebido){
-        Usuario usuario = usuarioService.salvarUsuario(modelMapper.map(cadastroRecebido,Usuario.class));
+    public ExibirUsuarioDTO cadastrarUsuario(@RequestBody CadastroUsuarioDTO cadastroRecebido) {
+        Usuario usuario = usuarioService.salvarUsuario(modelMapper.map(cadastroRecebido, Usuario.class));
 
-        return modelMapper.map(usuario,ExibirUsuarioDTO.class);
+        return modelMapper.map(usuario, ExibirUsuarioDTO.class);
     }
 
+    @GetMapping
+    public List<ExibirDetalheJogoDTO> exibirJogos() {
+        List<ExibirDetalheJogoDTO> listaDeJogos = new ArrayList<>();
 
+        for (Jogo jogo : usuarioService.exibirTodosJogos()) {
+            ExibirDetalheJogoDTO exibirDetalheJogoDTO = modelMapper.map(jogo, ExibirDetalheJogoDTO.class);
+            listaDeJogos.add(exibirDetalheJogoDTO);
+        }
+        return listaDeJogos;
+    }
 }
