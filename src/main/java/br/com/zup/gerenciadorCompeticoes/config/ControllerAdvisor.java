6ee500be9d26cf62434532ef8d59b5a4b02a40ad
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-30 13:34:31 -0300

    Adicionar método para manipular erros de validação

--- src/main/java/br/com/zup/gerenciadorCompeticoes/config/ControllerAdvisor.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/config/ControllerAdvisor.java
@@ -1,10 +1,28 @@
 package br.com.zup.gerenciadorCompeticoes.config;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvisor {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<MensagemErro> manipularErroValidacao(MethodArgumentNotValidException exception){
+        List<MensagemErro> errosValidacao = new ArrayList<>();
+        for (FieldError referencia: exception.getFieldErrors()) {
+            MensagemErro mensagemErro = new MensagemErro(referencia.getDefaultMessage());
+            errosValidacao.add(mensagemErro);
+        }
 
+        return errosValidacao;
+    }
 
 }
