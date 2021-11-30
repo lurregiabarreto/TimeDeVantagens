bf10e58b2261092f97484bb95d1ad11668bc1b4a
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-30 13:33:31 -0300

    Criar classe controller advisor

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/config/ControllerAdvisor.java
@@ -0,0 +1,10 @@
+package br.com.zup.gerenciadorCompeticoes.config;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvisor {
+
+
+
+}
