6811db3ab36cdf56ec25a57c7cfee1f7d4bbdb0e
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-30 12:04:10 -0300

    adicionar método pesquisar usuario id

--- src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioService.java
@@ -1,5 +1,6 @@
 package br.com.zup.gerenciadorCompeticoes.usuario;
 
+import br.com.zup.gerenciadorCompeticoes.exceptions.JogoNaoEncontradoException;
 import br.com.zup.gerenciadorCompeticoes.jogo.Jogo;
 import br.com.zup.gerenciadorCompeticoes.jogo.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -19,26 +21,26 @@ public class UsuarioService {
 
     public Usuario salvarUsuario(Usuario usuarioRecebido) {
         usuarioRecebido.setPontos(0);
+
         return usuarioRepository.save(usuarioRecebido);
     }
 
-    public List<Jogo> exibirTodosJogos (){
+    public List<Jogo> exibirTodosJogos() {
         List<Jogo> jogos = (List<Jogo>) jogoRepository.findAll();
+
         return jogos;
-
-    }
-
     }
 
 
+    public Jogo pesquisarUsuarioPorID(int id) {
+        Optional<Jogo> jogoId = jogoRepository.findById(id);
 
+        if (jogoId.isPresent()) {
+            return jogoId.get();
+        }
 
+        throw new JogoNaoEncontradoException("Jogo não encontrado");
 
+    }
 
-
-
-
-
-
-
-
+}
