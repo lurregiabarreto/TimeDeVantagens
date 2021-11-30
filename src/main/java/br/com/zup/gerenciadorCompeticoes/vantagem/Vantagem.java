b6dd1978ed1c48f5694c12bffd33979891a27247
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-30 09:34:44 -0300

    Sobreescrever métodos equals e hashCode

--- src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/Vantagem.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/Vantagem.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "vantagem")
@@ -32,5 +33,19 @@ public class Vantagem {
     public void setPontos(int pontos) {
         this.pontos = pontos;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vantagem vantagem = (Vantagem) o;
+        return pontos == vantagem.pontos && Objects.equals(beneficio, vantagem.beneficio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(beneficio, pontos);
+    }
+
 }
 
