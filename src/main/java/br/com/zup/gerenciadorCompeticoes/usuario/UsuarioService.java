70532d71f639eaf7eddcc8ad03de7e2c2efc9c20
Bárbara Ann Lima <babiplima@gmail.com>
2021-11-30 11:51:01 -0300

    Adicionar metodo Exibir Todos Jogos

--- src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioService.java
@@ -1,18 +1,44 @@
 package br.com.zup.gerenciadorCompeticoes.usuario;
 
+import br.com.zup.gerenciadorCompeticoes.jogo.Jogo;
+import br.com.zup.gerenciadorCompeticoes.jogo.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UsuarioService {
 
     @Autowired
     UsuarioRepository usuarioRepository;
+    @Autowired
+    JogoRepository jogoRepository;
 
 
-    public Usuario salvarUsuario(Usuario usuarioRecebido){
+    public Usuario salvarUsuario(Usuario usuarioRecebido) {
         usuarioRecebido.setPontos(0);
         return usuarioRepository.save(usuarioRecebido);
     }
 
-}
+    public List<Jogo> exibirTodosJogos (){
+        List<Jogo> jogos = (List<Jogo>) jogoRepository.findAll();
+        return jogos;
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
