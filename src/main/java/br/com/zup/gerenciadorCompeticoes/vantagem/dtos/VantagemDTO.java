526be1bc1f2c2bcf3edc2e7abf4a8b4406d50ec8
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-30 14:10:01 -0300

    Adicionar anotações de validação para vantagem dto

--- src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/dtos/VantagemDTO.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/vantagem/dtos/VantagemDTO.java
@@ -1,10 +1,18 @@
 package br.com.zup.gerenciadorCompeticoes.vantagem.dtos;
 
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.*;
+
 public class VantagemDTO {
 
+    @NotBlank(message = "Benefício é obrigatório")
+    @Size(min = 2, message = "O benefício deve conter no mínimo 2 caracteres")
     private String beneficio;
-    private double pontos;
+    @NotNull(message = "Pontos é obrigatório")
+    @Range(min = 5,max = 350)
+    private int pontos;
 
     public VantagemDTO() {
     }
@@ -17,11 +25,11 @@ public class VantagemDTO {
         this.beneficio = beneficio;
     }
 
-    public double getPontos() {
+    public int getPontos() {
         return pontos;
     }
 
-    public void setPontos(double pontos) {
+    public void setPontos(int pontos) {
         this.pontos = pontos;
     }
 
