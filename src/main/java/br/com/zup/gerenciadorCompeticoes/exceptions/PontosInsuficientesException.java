c239c23de0f2b3f5b0650d136ade12e05c184a30
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-01 16:37:47 -0300

    Criar classe exceção para pontos insuficientes

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/exceptions/PontosInsuficientesException.java
@@ -0,0 +1,9 @@
+package br.com.zup.gerenciadorCompeticoes.exceptions;
+
+public class PontosInsuficientesException extends RuntimeException{
+
+    public PontosInsuficientesException(String message) {
+        super(message);
+    }
+
+}
