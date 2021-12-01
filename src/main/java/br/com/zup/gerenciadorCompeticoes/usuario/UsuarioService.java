4f541b40b82832d62c67df6b840befa33abbcdb2
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-01 11:46:13 -0300

    Refatorar métodos da service usurário

--- src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/usuario/UsuarioService.java
@@ -2,14 +2,12 @@ package br.com.zup.gerenciadorCompeticoes.usuario;
 
 import br.com.zup.gerenciadorCompeticoes.exceptions.JogoNaoEncontradoException;
 import br.com.zup.gerenciadorCompeticoes.exceptions.UsuarioNEncontrado;
-import br.com.zup.gerenciadorCompeticoes.exceptions.IdInvalid;
 import br.com.zup.gerenciadorCompeticoes.jogo.Jogo;
 import br.com.zup.gerenciadorCompeticoes.jogo.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UsuarioService {
@@ -32,43 +30,34 @@ public class UsuarioService {
         return jogos;
     }
 
-
-    public Jogo pesquisarUsuarioPorID(int id) {
+    public Jogo pesquisarJogoPorID(int id) {
         Optional<Jogo> jogoId = jogoRepository.findById(id);
 
-        if (jogoId.isPresent()) {
-            return jogoId.get();
+        if (jogoId.isEmpty()) {
+            throw new JogoNaoEncontradoException("Este Id de Jogo é inválido,jogo não foi encontrado");
         }
 
-        throw new JogoNaoEncontradoException("Jogo não encontrado");
-
+        return jogoId.get();
     }
 
-
     public Usuario checkinUsuario(String email, int id) {
-        Optional<Jogo> verificarJogo = jogoRepository.findById(id);
-        if (verificarJogo.isEmpty()) {
-            throw new IdInvalid("Este Id de Jogo é inválido, não foi encontrado");
-        }
+        Jogo jogo = pesquisarJogoPorID(id);
+        var pontosCheckin = 5;
 
         Usuario usuarioAtualizado = buscarUsuarioId(email);
-        usuarioAtualizado.setPontos(usuarioAtualizado.getPontos()+5);
+        usuarioAtualizado.setPontos(usuarioAtualizado.getPontos() + pontosCheckin);
         usuarioRepository.save(usuarioAtualizado);
 
         return usuarioAtualizado;
     }
 
     public Usuario buscarUsuarioId(String email) {
-        Optional<Usuario>usuarioBuscar = usuarioRepository.findById(email);
-        if(usuarioBuscar.isEmpty()){
-            throw new UsuarioNEncontrado( "Este usuário não foi encontrado");
+        Optional<Usuario> usuarioBuscar = usuarioRepository.findById(email);
+        if (usuarioBuscar.isEmpty()) {
+            throw new UsuarioNEncontrado("Este usuário não foi encontrado");
         }
+
         return usuarioBuscar.get();
     }
-
-
-
-
-
 
 }
