fe4792612be046f8ca244fcf7c5826eec5e16140
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-02 13:30:25 -0300

    Criar repository para voucher

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/voucher/VoucherRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.gerenciadorCompeticoes.voucher;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VoucherRepository extends CrudRepository<Voucher, String> {
+
+}
