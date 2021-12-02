460f6b935643a6e1c98bf015e7c28158704da150
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-02 09:59:53 -0300

    Criar exceção para código inválido

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/exceptions/CodigoInvalidoException.java
@@ -0,0 +1,9 @@
+package br.com.zup.gerenciadorCompeticoes.exceptions;
+
+public class CodigoInvalidoException extends RuntimeException {
+
+    public CodigoInvalidoException(String message) {
+        super(message);
+    }
+
+}
