08f8dddc213c0b817310d55fe8a99a5f283df8f8
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-03 11:34:04 -0300

    Criar repository para administrador

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/adm/AdmRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.gerenciadorCompeticoes.adm;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdmRepository extends CrudRepository<Administrador,Integer> {
+}
