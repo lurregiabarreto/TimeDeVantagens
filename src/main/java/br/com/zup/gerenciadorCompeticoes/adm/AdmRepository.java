044a326a89caf53f23d4219b6b302e85f13af539
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-03 17:56:45 -0300

    Criar README

--- src/main/java/br/com/zup/gerenciadorCompeticoes/adm/AdmRepository.java
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/adm/AdmRepository.java
@@ -2,5 +2,6 @@ package br.com.zup.gerenciadorCompeticoes.adm;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface AdmRepository extends CrudRepository<Administrador,Integer> {
+public interface AdmRepository extends CrudRepository<Administrador, Integer> {
+
 }
