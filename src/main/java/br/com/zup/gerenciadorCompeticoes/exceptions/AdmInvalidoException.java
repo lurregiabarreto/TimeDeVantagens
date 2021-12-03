6d0ee33af77287659be3c70278db7ec376a5c175
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-12-03 11:39:07 -0300

    Criar classe para exceção de administrador inválido

---
+++ src/main/java/br/com/zup/gerenciadorCompeticoes/exceptions/AdmInvalidoException.java
@@ -0,0 +1,9 @@
+package br.com.zup.gerenciadorCompeticoes.exceptions;
+
+public class AdmInvalidoException extends RuntimeException {
+
+    public AdmInvalidoException(String message) {
+        super(message);
+    }
+
+}
